76b60e0f035c26f98096ffea3a2eccaa1997519b
harley <harley@localhost.localdomain>
2024-09-17 15:12:51 -0300

    fix: debug local

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# ################################################
+# Requerimentos
+# ################################################
 # check 7zip im path
 if which 7z >/dev/null 2>&1; then
     echo "Installing ssh scripts..."
@@ -8,16 +11,19 @@ else
     exit 1
 fi
 
+# ################################################
+# Prepara diretorios e faz limpa tudo
+# ################################################
 rm -rf "$HOME/.ssh"
 git clone https://github.com/harleysad/ssh.git ~/.ssh  
-
 # mkdir "$HOME/.ssh"
 # cp -R ./* "$HOME/.ssh"
-
 chmod 700 $HOME/.ssh/*.sh
 
+# ################################################
+# Chaves publicas 
+# ################################################
 folder="$HOME/.ssh/public-keys"
-
 #enable public keys to login
 for file in $HOME/.ssh/public-keys/*.pub; do
   echo "Installing public key $file"
@@ -25,23 +31,31 @@ for file in $HOME/.ssh/public-keys/*.pub; do
        cat "$file" >>  "$HOME/.ssh/authorized_keys"
   fi
 done
+chmod -R 700 $HOME/.ssh/public-keys
 
-
+# ################################################
+# Descompacta chaves privadas
+# ################################################
 # 7z a -p -mhe=on private-keys  private-keys
-
+# Verifica se existe chaves para configurar
 if [ -f "$HOME/.ssh/private-keys.7z" ]; then 
   echo "Starting config private keys"
 else
   echo "No private keys"
   exit 1
 fi
-
+# Descompacta as chaves
 7z x $HOME/.ssh/private-keys.7z -o$HOME/.ssh/
 if [ $? -ne 0 ]; then
   echo "Error extracting: private-keys.7z"
   exit 1
 fi
+chmod -R 700 $HOME/.ssh/private-keys
 
+# ################################################
+# Salva o script para carregar o ssh-agent
+# no .bashrc
+# ################################################
 SCRIPT_TO_SOURCE="source $HOME/.ssh/load-private-keys.sh"
 TARGET_FILE="$HOME/.bashrc"
 
@@ -52,6 +66,3 @@ else
     echo "$SCRIPT_TO_SOURCE" >> "$TARGET_FILE"
     echo "['$SCRIPT_TO_SOURCE'] added to $TARGET_FILE."
 fi
-
-chmod -R 700 $HOME/.ssh/private-keys
-chmod -R 700 $HOME/.ssh/public-keys
--- load-private-keys.sh
+++ load-private-keys.sh
@@ -5,8 +5,8 @@ SSH_ENV="$HOME/.ssh/agent-environment"
 
 # Função para iniciar o ssh-agent
 start_agent() {
+    rm $SSH_ENV > /dev/null 2>&1
     # mata os processos perdidos ( inda não sei as implicações )
-    rm $SSH_ENV
     pkill ssh-agent 
     echo $(ssh-agent -s) > "$SSH_ENV"
     chmod 600 "$SSH_ENV"
@@ -14,16 +14,19 @@ start_agent() {
 
 #verifica se o arquivo com ultima config esta presente
 if [ -f "$SSH_ENV" ]; then
+    # carrega as configurações encontradas
+    source "$SSH_ENV" > /dev/null
     # verifica se o pid do agente carregado esta rodando
     # se não tem vai carregar um novo agente
     if ! ps -p $SSH_AGENT_PID > /dev/null 2>&1; then
         start_agent
     fi
     # carrega o agente do arquivo compartilhado no usuario
-    source "$SSH_ENV" > /dev/null
+    
 else
     start_agent
 fi
+source "$SSH_ENV" > /dev/null
 
 #carrega as chaves 
 for file in ~/.ssh/private-keys/*; do
@@ -31,4 +34,3 @@ for file in ~/.ssh/private-keys/*; do
        ssh-add "$file" > /dev/null 2>&1
   fi
 done
-
