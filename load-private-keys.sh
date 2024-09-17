bb64e3cf3ee489c510daea000dd4f00cd4b1c8aa
harley <harley@localhost.localdomain>
2024-09-17 00:13:50 -0300

    fix: ssh-agent on hand

--- load-private-keys.sh
+++ load-private-keys.sh
@@ -5,22 +5,26 @@ SSH_ENV="$HOME/.ssh/agent-environment"
 
 # Função para iniciar o ssh-agent
 start_agent() {
-    echo "Iniciando novo ssh-agent..."
+    # mata os processos perdidos ( inda não sei as implicações )
+    pkill ssh-agent 
     eval $(ssh-agent -s) > "$SSH_ENV"
     chmod 600 "$SSH_ENV"
 }
 
-# Carregar variáveis de ambiente salvas
+#verifica se o arquivo com ultima config esta presente
 if [ -f "$SSH_ENV" ]; then
-    source "$SSH_ENV" > /dev/null
-    # Verifica se o agente ainda está rodando
+    # verifica se o pid do agente carregado esta rodando
+    # se não tem vai carregar um novo agente
     if ! ps -p $SSH_AGENT_PID > /dev/null 2>&1; then
         start_agent
     fi
+    # carrega o agente do arquivo compartilhado no usuario
+    source "$SSH_ENV" > /dev/null
 else
     start_agent
 fi
-#carrega as chaves privadas
+
+#carrega as chaves 
 for file in ~/.ssh/private-keys/*; do
   if [ -f "$file" ]; then 
        ssh-add "$file" > /dev/null 2>&1
